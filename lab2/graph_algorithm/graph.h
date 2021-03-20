b482370dcb8e617d89662453c85098b4a0747862
vik-ivahnuk <vik.ivahnuk@gmail.com>
2021-03-20 17:23:31 +0200

    add methods for classes Graph_Node_Iterator

--- lab2/graph_algorithm/graph.h
+++ lab2/graph_algorithm/graph.h
@@ -9,16 +9,27 @@ template <typename T,typename U>
 class Graph_Node_Iterator{
     typedef typename  pair<T,pair<int,int>>:: graph_node_iterator  Node ;
 public :
+
     Graph_Node_Iterator(U matrix){
         this->matrix=matrix;
+        First_absolute();
     }
 
-    void First(){
+    void First_absolute(){
 
         node={matrix.get_length(0,0),{0,0}};
     };
 
-    void Next(){
+    void First_horizontal(){
+
+        node={matrix.get_length(node.second.first,0),{node.second.first,0}};
+    };
+    void First_verticall(){
+
+        node={matrix.get_length(0,node.second.second),{0,node.second.second}};
+    };
+
+    void Next_absolute(){
         if(matrix.get_lenght()-1==node.second.first && matrix.get_lenght()-1 >node.second.second ){
             node.first=matrix.get_length(0,++node.second.second);
             node.second.first=0;
@@ -29,12 +40,40 @@ public :
          else return;
     };
 
-    bool isDone(){
+    void Next_horizontal(){
+        if( matrix.get_lenght()-1 ==node.second.second ){
+            node.first=matrix.get_length(0,++node.second.second);
+            node.second.first=0;
+        }
+         if( matrix.get_lenght()-1 >node.second.second ){
+         node.first=matrix.get_length(node.second.first,++node.second.second);
+         }
+         else return;
+    };
+
+    void Next_verticall(){
+
+
+         if( matrix.get_lenght()-1 >node.second.first ){
+         node.first=matrix.get_length(++node.second.first,node.second.second);
+         }
+         else return;
+    };
+
+    bool isDone_absolute(){
         return node.second.first==matrix.size_of_graph()-1 && node.second.second==matrix.size_of_graph()-1;
     };
 
-    Node Current(){
-        return node;
+    bool isDone_horizontal(){
+        return  node.second.second==matrix.size_of_graph()-1;
+    };
+    bool isDone_verticall(){
+        return  node.second.first==matrix.size_of_graph()-1;
+    };
+
+
+    T Current(){
+        return node.first;
     };
 
 private:
@@ -67,6 +106,8 @@ private:
     vector<vector<T>> mtrx;
 
 };
+
+
 
 
 #endif // GRAPH_H
