675184c01dd24cea34ddd03989256f52536381b1
Vladyslav Korosko <vlad.korosko1@gmail.com>
2021-03-22 20:26:52 +0200

    Added Menu

--- build-lab2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-lab2-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -13,6 +13,7 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -22,6 +23,11 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QPushButton *IntegerButton;
+    QPushButton *DoubleButton;
+    QPushButton *StringButton;
+    QPushButton *ExitButton;
+    QPushButton *BackButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -29,12 +35,33 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(825, 585);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        IntegerButton = new QPushButton(centralwidget);
+        IntegerButton->setObjectName(QString::fromUtf8("IntegerButton"));
+        IntegerButton->setGeometry(QRect(260, 40, 251, 81));
+        DoubleButton = new QPushButton(centralwidget);
+        DoubleButton->setObjectName(QString::fromUtf8("DoubleButton"));
+        DoubleButton->setGeometry(QRect(260, 170, 251, 81));
+        StringButton = new QPushButton(centralwidget);
+        StringButton->setObjectName(QString::fromUtf8("StringButton"));
+        StringButton->setGeometry(QRect(260, 300, 251, 81));
+        ExitButton = new QPushButton(centralwidget);
+        ExitButton->setObjectName(QString::fromUtf8("ExitButton"));
+        ExitButton->setGeometry(QRect(260, 430, 251, 81));
+        BackButton = new QPushButton(centralwidget);
+        BackButton->setObjectName(QString::fromUtf8("BackButton"));
+        BackButton->setGeometry(QRect(260, 430, 251, 81));
         MainWindow->setCentralWidget(centralwidget);
+        BackButton->raise();
+        IntegerButton->raise();
+        DoubleButton->raise();
+        StringButton->raise();
+        ExitButton->raise();
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 825, 26));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -48,6 +75,11 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        IntegerButton->setText(QCoreApplication::translate("MainWindow", "Integer Graph", nullptr));
+        DoubleButton->setText(QCoreApplication::translate("MainWindow", "Double Graph", nullptr));
+        StringButton->setText(QCoreApplication::translate("MainWindow", "String Graph", nullptr));
+        ExitButton->setText(QCoreApplication::translate("MainWindow", "Exit", nullptr));
+        BackButton->setText(QCoreApplication::translate("MainWindow", "Back", nullptr));
     } // retranslateUi
 
 };
